bd45660a86ea7f02c308a1cac7cbb569cf45fbe7
Richard Moore <richard.moore@yozobi.com>
2021-10-03 21:24:35 +0100

    header css fix

--- src/components/Header/UserSettingsBtn.tsx
+++ src/components/Header/UserSettingsBtn.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import { ReactComponent as DropdownIcon } from 'assets/triangle.svg';
 
 export const UserSettingsBtn: FC = () => (
-  <div className="flex items-center justify-center px-5 overflow-hidden font-medium rounded-r-lg shadow-inner select-none md:rounded-lg bg-mirage-600 hover:cursor-pointer group">
+  <div className="flex items-center justify-center px-5 overflow-hidden font-medium rounded-l-lg shadow-inner select-none md:rounded-lg bg-mirage-600 hover:cursor-pointer group">
     <img
       src="/user-example.png"
       className="flex-shrink-0 w-8 h-8 border-2 rounded-full md:w-12 md:h-12 border-mirage-100"
