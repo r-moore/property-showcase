a37a072601c4e50abf14d8f6ee2f7b0412fa4402
Richard Moore <richard.moore@yozobi.com>
2021-10-03 22:47:52 +0100

    change grid cols

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import { motion } from 'framer-motion';
 
 export const CardList: FC = ({ children }) => (
-  <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
+  <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
     {children}
   </div>
 );
