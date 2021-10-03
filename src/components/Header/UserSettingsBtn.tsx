277231e574246c8175eed1f64e36aca530b84faa
Richard Moore <richard.moore@yozobi.com>
2021-10-03 22:55:28 +0100

    user menu icon

--- src/components/Header/UserSettingsBtn.tsx
+++ src/components/Header/UserSettingsBtn.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { ReactComponent as DropdownIcon } from 'assets/triangle.svg';
+import { ReactComponent as HamburgerIcon } from 'assets/hamburger.svg';
 
 export const UserSettingsBtn: FC = () => (
   <div className="flex items-center justify-center px-5 overflow-hidden font-medium rounded-l-lg shadow-inner select-none md:rounded-lg bg-mirage-600 hover:cursor-pointer group">
@@ -7,6 +7,6 @@ export const UserSettingsBtn: FC = () => (
       src="/user-example.png"
       className="flex-shrink-0 w-8 h-8 border-2 rounded-full md:w-12 md:h-12 border-mirage-100"
     />
-    <DropdownIcon className="flex-shrink-0 w-auto h-2 ml-3 -mb-px text-gray-300" />
+    <HamburgerIcon className="flex-shrink-0 w-auto h-6 ml-3 -mb-px text-gray-300" />
   </div>
 );
